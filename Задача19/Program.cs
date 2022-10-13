5ff4fd3cb7905201b8714dd044109abe10f65278
Alexander Prilepin <aleksander930@list.ru>
2022-10-13 22:25:05 +0300

    Lesson3

---
+++ Задача19/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 19
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+bool number(int n)
+{
+    return (n % 10 == n / 10000) && ((n % 100) / 10 == (n / 1000) % 10);
+}
+
+bool num = number(n);
+if (num)
+{
+    Console.WriteLine($"{n} да");
+}
+else Console.WriteLine($"{n} нет");
---
+++ Задача21/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 21
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите a1 ");
+int a1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите b1 ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите c1 ");
+int c1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите a2 ");
+int a2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите b2 ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите c2 ");
+int c2 = Convert.ToInt32(Console.ReadLine());
+
+double Distance(int arga1, int argb1, int argc1, int arga2, int argb2, int argc2)
+{
+    double result = Math.Sqrt((Math.Pow(arga2 - arga1, 2)) + (Math.Pow(argb2 - argb1, 2)) + (Math.Pow(argc2 - argc1, 2)));
+    return result;
+}
+
+double res = Distance(a1, b1, c1, a2, b2, c2);
+Console.WriteLine(Math.Round(res, 2, MidpointRounding.ToZero));
